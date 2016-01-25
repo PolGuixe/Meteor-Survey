8dbc7268ba59c0904dbe57ce22df31e91996f741
PolGuixe <pol.guixe@gmail.com>
2016-01-25 12:40:16 +0100

    basic router and pages

---
+++ app/lib/router.js
@@ -0,0 +1,9 @@
+Router.configure({
+  layoutTemplate: 'layout',
+  notFoundTemplate: 'notFound',
+});
+
+//TODO-check if is better to define routes by functions -- Pol G.
+Router.route('/', {
+  name: 'landingPage'
+});
